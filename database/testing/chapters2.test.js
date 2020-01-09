6febcebcee311238880348bd7c8d75ace2c76845
azrap <azraap@gmail.com>
2020-01-08 22:41:26 -0800

    working on chapters backend testing

---
+++ database/testing/chapters2.test.js
@@ -0,0 +1,80 @@
+const request = require("supertest");
+const server = require("../../server");
+
+const db = require("../../config/dbConfig.js");
+const Chapters = require("../../models/chapters-model.js");
+
+let token;
+
+beforeAll(done => {
+  request(server)
+    .post("/login")
+    .send({
+      username: "kevin",
+      password: "password"
+    })
+    .end((err, res) => {
+      token = res.body.token;
+      done();
+    });
+});
+
+test("It responds with JSON", () => {
+  return request(server)
+    .get("/")
+    .set("Authorization", `Bearer ${token}`)
+    .then(res => {
+      expect(res.statusCode).toBe(200);
+      expect(res.type).toBe("application/json");
+    });
+});
+
+describe("chapter api route", () => {
+  beforeEach(async () => {
+    await db("chapters").del();
+  });
+
+  describe("GET /api/chapter", () => {
+    test("It responds with JSON", () => {
+      return request(server)
+        .get("/api/chapter")
+        .set("Authorization", `Bearer ${token}`)
+        .then(res => {
+          expect(res.statusCode).toBe(200);
+          expect(res.type).toBe("application/json");
+        });
+    });
+
+    it("should return 200", async () => {
+      const res = await request(server).get("/api/chapter");
+      expect(res.status).toBe(200);
+    });
+
+    it("should return json type", async () => {
+      const res = await request(server).get("/api/chapter");
+      expect(res.type).toBe("application/json");
+    });
+
+    it("should insert chapters into the db", async () => {
+      await Chapters.addChapter({
+        location: "San Antonio, TX",
+        numvolunteers: 13,
+        longitude: -80.843124,
+        latitude: 35.227085
+      });
+      await Chapters.addChapter({
+        location: "Norfolk, VA",
+        numvolunteers: 52,
+        longitude: -80.843124,
+        latitude: 35.227085
+      });
+
+      const chapters = await db("chapters");
+
+      expect(chapters).toHaveLength(2);
+      expect(chapters[0].location).toBe("San Antonio, TX");
+      expect(chapters[1].location).toBe("Norfolk, VA");
+      expect(chapters[1].numvolunteers).toBe(52);
+    });
+  });
+});
--- server.test.js
+++ server.test.js
@@ -1,13 +1,20 @@
-const supertest = require('supertest');
-const server = require('./server');
+const supertest = require("supertest");
+const server = require("./server");
 
-describe('server.js', () => {
-  describe('get', () => {
-    it('should return status 200', async () => {
-      const user = await supertest(server)
-        .get('/')
-        .expect(200);
-    });
+describe("server.js", () => {
+  it("should set the test env", () => {
+    expect(process.env.DB_ENV).toBe("testing");
   });
 });
 
+describe("GET /", () => {
+  it("should return status 200", async () => {
+    const res = await supertest(server).get("/");
+    expect(res.status).toBe(200);
+  });
+
+  it("should return json", async () => {
+    const res = await supertest(server).get("/");
+    expect(res.type).toBe("application/json");
+  });
+});
